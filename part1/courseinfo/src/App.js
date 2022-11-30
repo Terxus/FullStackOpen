87a1011295219403574fda1ad355497bb2a85dce
Terxus <7191599+Terxus@users.noreply.github.com>
2022-11-30 14:07:40 +0100

    exercis 1.1 + 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = (props) => {
+  return (
+      <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} excersises={props.excercise1} />
+      <Part name={props.part2} excersises={props.excercise2} />
+      <Part name={props.part3} excersises={props.excercise3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+      <p>{props.name} {props.excersises}</p>
+  )
+}
+
+const Total = (props) => {
+  return(
+      <p>Number of excersises {props.total}</p>
+  )
+}
 
 function App() {
+  const course = 'Half Stack Application Development';
+  const part1 = 'Fundamentals of React';
+  const excercise1 = 10;
+  const part2 = 'Using props to pass data';
+  const excercise2 = 7;
+  const part3 = 'State of the component';
+  const excercise3 = 14;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header course={course}/>
+      <Content part1={part1} excercise1={excercise1}
+               part2={part2} excercise2={excercise2}
+               part3={part3} excercise3={excercise3} />
+      <Total total={excercise1 + excercise2 + excercise3} />
     </div>
   );
 }
